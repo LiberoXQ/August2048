e4e68a8dd6a8da787ebe7dc08e9c8b76149cc6ef
XQ <chinaxq1992@gmail.com>
2014-10-19 11:29:58 +0800

    First UI

--- src/xq/august2048/activity/MainActivity.java
+++ src/xq/august2048/activity/MainActivity.java
@@ -7,18 +7,26 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
-	TextView title, score, scores, best, bests, annoncement;
-	Button start;
-	GridView grid;
+	private TextView title, score, scores, best, bests, annoncement;
+	private Button start;
+	private GridView gridView;
+	
+	private GridAdapter gridAdapter;
+	private Context con;
+	private int[] img;
+	private long mExitTime;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -28,8 +36,35 @@ public class MainActivity extends Activity
 		// Remove Title
 //		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		initView();
-		int[] img = new int[]
+		
+		/*
+		 * @author http://www.eoeandroid.com/thread-163834-1-1.html
+         * 必须事先在assets底下创建一fonts文件夹 并放入要使用的字体文件(.ttf)
+         * 并提供相对路径给creatFromAsset()来创建Typeface对象
+         */
+        Typeface fontFace = Typeface.createFromAsset(getAssets(),
+                        "fonts/Kim's GirlType.ttf");
+        // 字体文件必须是true type font的格式(ttf)；
+        // 当使用外部字体却又发现字体没有变化的时候(以 Droid Sans代替)，通常是因为
+        // 这个字体android没有支持,而非你的程序发生了错误
+		
+        title = (TextView)findViewById(R.id.activity_main_title);
+        title.setTypeface(fontFace);
+		score = (TextView)findViewById(R.id.activity_main_score);
+		score.setTypeface(fontFace);
+		scores = (TextView)findViewById(R.id.activity_main_scores);
+		scores.setTypeface(fontFace);
+		best = (TextView)findViewById(R.id.activity_main_best);
+		best.setTypeface(fontFace);
+		bests = (TextView)findViewById(R.id.activity_main_bests);
+		bests.setTypeface(fontFace);
+		annoncement = (TextView)findViewById(R.id.activity_main_annoncement);
+		annoncement.setTypeface(fontFace);
+		start = (Button)findViewById(R.id.activity_main_start);
+		start.setTypeface(fontFace);
+		gridView = (GridView)findViewById(R.id.activity_main_grid);
+		
+		img = new int[]
 				{
 				R.drawable.blank,//0
 				R.drawable.blank,//1
@@ -48,22 +83,31 @@ public class MainActivity extends Activity
 				R.drawable.blank,//14
 				R.drawable.blank,//15
 				};
-		Context con = this;
+		con = this;
+		gridAdapter = new GridAdapter(con, img);
 		//Remove Click Animation
-		grid.setSelector(new ColorDrawable(Color.TRANSPARENT));
-		grid.setAdapter(new GridAdapter(con, img));
+		gridView.setSelector(new ColorDrawable(Color.TRANSPARENT));
+		gridView.setAdapter(gridAdapter);
 	}
-	
-	void initView()
+
+	public boolean onKeyDown(int keyCode, KeyEvent event) 
 	{
-		title = (TextView)findViewById(R.id.activity_main_title);
-		score = (TextView)findViewById(R.id.activity_main_score);
-		scores = (TextView)findViewById(R.id.activity_main_scores);
-		best = (TextView)findViewById(R.id.activity_main_best);
-		bests = (TextView)findViewById(R.id.activity_main_bests);
-		annoncement = (TextView)findViewById(R.id.activity_main_annoncement);
-		start = (Button)findViewById(R.id.activity_main_start);
-		grid = (GridView)findViewById(R.id.activity_main_grid);
+        if (keyCode == KeyEvent.KEYCODE_BACK) 
+        {
+                if ((System.currentTimeMillis() - mExitTime) > 2000) 
+                {
+//                    Object mHelperUtils;
+                	Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                    mExitTime = System.currentTimeMillis();
+
+                } 
+                else 
+                {
+                	finish();
+                	System.exit(0);
+                }
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
 	}
-	
 }
--- src/xq/august2048/adapter/GridAdapter.java
+++ src/xq/august2048/adapter/GridAdapter.java
@@ -1,10 +1,19 @@
 package xq.august2048.adapter;
 
+import xq.august2048.R;
 import android.content.Context;
-import android.util.DisplayMetrics;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff; 
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
@@ -46,15 +55,45 @@ public class GridAdapter extends BaseAdapter
 		// TODO Auto-generated method stub
 		ImageView iv = null;
 		iv = new ImageView(context);
-		iv.setImageResource(image[position]);
-		//Get Device Parameter
-		DisplayMetrics dm = context.getResources().getDisplayMetrics();
-//		LayoutParams lp = iv.getLayoutParams();
-//		if(lp == null) lp = new LayoutParams(100,100);
-//		lp.height = (int)((dm.widthPixels - 50) / 4);
-//	    lp.width = (int)((dm.widthPixels - 50) / 4);
-//	    iv.setLayoutParams(lp);
+//		iv.setImageResource(image[position]);
+		Bitmap photo = BitmapFactory.decodeResource(context.getResources(), R.drawable.blank);
+        iv.setImageBitmap(createFramedPhoto(photo.getWidth(),photo.getHeight(),photo,(int)(photo.getWidth()/4)));
 		return iv;
 	}
+	
+	/**
+    * @author http://blog.chinaunix.net/uid-20771867-id-3260250.html
+    * @param x 图像的宽度
+    * @param y 图像的高度
+    * @param image 源图片
+    * @param outerRadiusRat 圆角的大小
+    * @return 圆角图片
+    */
+   Bitmap createFramedPhoto(int x, int y, Bitmap image, float outerRadiusRat) 
+   {
+       //根据源文件新建一个darwable对象
+       Drawable imageDrawable = new BitmapDrawable(image);
+
+       // 新建一个新的输出图片
+       Bitmap output = Bitmap.createBitmap(x, y, Bitmap.Config.ARGB_8888);
+       Canvas canvas = new Canvas(output);
+
+       // 新建一个矩形
+       RectF outerRect = new RectF(0, 0, x, y);
+
+       // 产生一个红色的圆角矩形
+       Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+       paint.setColor(Color.RED);
+       canvas.drawRoundRect(outerRect, outerRadiusRat, outerRadiusRat, paint);
+
+       // 将源图片绘制到这个圆角矩形上
+       paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+       imageDrawable.setBounds(0, 0, x, y);
+       canvas.saveLayer(outerRect, paint, Canvas.ALL_SAVE_FLAG);
+       imageDrawable.draw(canvas);
+       canvas.restore();
+
+       return output;
+   }
 	
 }
