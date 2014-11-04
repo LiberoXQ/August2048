a9ca9672848c9e7208a1a7e07de3ec36dd9dbfda
XQ <chinaxq1992@gmail.com>
2014-11-04 14:23:31 +0800

    Tiny modify

--- src/xq/august2048/activity/package-info.java
+++ src/xq/august2048/activity/package-info.java
@@ -11,19 +11,8 @@ package xq.august2048.activity;
 /**
  * MainActivity
  * 待办事项：
- * 保存状态
- * 不同屏幕尺寸字号调整
- * 微博分享
- * 优化性能
- * 美化界面
  * 调整字体包大小
  * 撰写注释和文档
- * 测试
- * 签名打包
- * 上传GitHub
- * 上架
- * 微博审核
- * 推广
  * @author XQ
  * @description The entrance Activity
  */
