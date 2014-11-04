b400fc84890fa9ad1ee883a7f25ff705ce4c88e0
XQ <chinaxq1992@gmail.com>
2014-11-04 21:13:05 +0800

    0.11

--- src/xq/august2048/entity/Cards.java
+++ src/xq/august2048/entity/Cards.java
@@ -43,16 +43,19 @@ public class Cards implements Serializable
 	public void stepLeft()
 	{
 		int[] temp = new int[4];
+		int[][] last = new int[4][];
 		int point = 0;
-		boolean flag = false;
+//		boolean flag = false;
 		for(int i = 0; i < 4; i++)
 		{
 			point = 0;
-			flag = false;
+//			flag = false;
+			last[i] = new int[4];
 			for(int j = 0; j < 4; j++)
 			{
-				if(flag == false)
-				{
+				last[i][j] = card[i][j];
+//				if(flag == false)
+//				{
 					if(card[i][j] == 0)
 						continue;
 					else if(point == 0)
@@ -63,19 +66,19 @@ public class Cards implements Serializable
 					else if(temp[point - 1] == card[i][j])
 					{
 						temp[point - 1]++;
-						flag = true;
+//						flag = true;
 					}
 					else
 					{
 						temp[point] = card[i][j];
 						point++;
 					}
-				}
-				else
-				{
-					temp[point] = card[i][j];
-					point++;
-				}
+//				}
+//				else
+//				{
+//					temp[point] = card[i][j];
+//					point++;
+//				}
 			}
 			for(int j = 0; j < 4; j++)
 			{
@@ -83,23 +86,41 @@ public class Cards implements Serializable
 				temp[j] = 0;
 			}
 		}
-//		rand = new Random(System.currentTimeMillis());
-		getRandom();
+		boolean change = false;
+		boolean full = true;
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				if(card[i][j] != last[i][j])
+					change = true;
+				if(card[i][j] == 0)
+					full = false;
+			}
+		}
+		if(change == true && full == false)
+		{
+			rand = new Random(System.currentTimeMillis());
+			getRandom();
+		}
 	}
 	
 	public void stepRight()
 	{
 		int[] temp = new int[4];
 		int point = 0;
-		boolean flag = false;
+//		boolean flag = false;
+		int[][] last = new int[4][];
 		for(int i = 0; i < 4; i++)
 		{
 			point = 3;
-			flag = false;
+//			flag = false;
+			last[i] = new int[4];
 			for(int j = 3; j >= 0; j--)
 			{
-				if(flag == false)
-				{
+				last[i][j] = card[i][j];
+//				if(flag == false)
+//				{
 					if(card[i][j] == 0)
 						continue;
 					else if(point == 3)
@@ -110,19 +131,19 @@ public class Cards implements Serializable
 					else if(temp[point + 1] == card[i][j])
 					{
 						temp[point + 1]++;
-						flag = true;
+//						flag = true;
 					}
 					else
 					{
 						temp[point] = card[i][j];
 						point--;
 					}
-				}
-				else
-				{
-					temp[point] = card[i][j];
-					point--;
-				}
+//				}
+//				else
+//				{
+//					temp[point] = card[i][j];
+//					point--;
+//				}
 			}
 			for(int j = 3; j >= 0; j--)
 			{
@@ -130,23 +151,41 @@ public class Cards implements Serializable
 				temp[j] = 0;
 			}
 		}
-//		rand = new Random(System.currentTimeMillis());
-		getRandom();		
+		boolean change = false;
+		boolean full = true;
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				if(card[i][j] != last[i][j])
+					change = true;
+				if(card[i][j] == 0)
+					full = false;
+			}
+		}
+		if(change == true && full == false)
+		{
+			rand = new Random(System.currentTimeMillis());
+			getRandom();
+		}		
 	}
 	
 	public void stepUp()
 	{
 		int[] temp = new int[4];
 		int point = 0;
-		boolean flag = false;
+//		boolean flag = false;
+		int[][] last = new int[4][];
 		for(int i = 0; i < 4; i++)
 		{
 			point = 0;
-			flag = false;
+//			flag = false;
+			last[i] = new int[4];
 			for(int j = 0; j < 4; j++)
 			{
-				if(flag == false)
-				{
+				last[i][j] = card[i][j];
+//				if(flag == false)
+//				{
 					if(card[j][i] == 0)
 						continue;
 					else if(point == 0)
@@ -157,19 +196,19 @@ public class Cards implements Serializable
 					else if(temp[point - 1] == card[j][i])
 					{
 						temp[point - 1]++;
-						flag = true;
+//						flag = true;
 					}
 					else
 					{
 						temp[point] = card[j][i];
 						point++;
 					}
-				}
-				else
-				{
-					temp[point] = card[j][i];
-					point++;
-				}
+//				}
+//				else
+//				{
+//					temp[point] = card[j][i];
+//					point++;
+//				}
 			}
 			for(int j = 0; j < 4; j++)
 			{
@@ -177,23 +216,41 @@ public class Cards implements Serializable
 				temp[j] = 0;
 			}
 		}
-//		rand = new Random(System.currentTimeMillis());
-		getRandom();
+		boolean change = false;
+		boolean full = true;
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				if(card[i][j] != last[i][j])
+					change = true;
+				if(card[i][j] == 0)
+					full = false;
+			}
+		}
+		if(change == true && full == false)
+		{
+			rand = new Random(System.currentTimeMillis());
+			getRandom();
+		}
 	}
 	
 	public void stepDown()
 	{
 		int[] temp = new int[4];
 		int point = 0;
-		boolean flag = false;
+//		boolean flag = false;
+		int[][] last = new int[4][];
 		for(int i = 0; i < 4; i++)
 		{
 			point = 3;
-			flag = false;
+//			flag = false;
+			last[i] = new int[4];
 			for(int j = 3; j >= 0; j--)
 			{
-				if(flag == false)
-				{
+				last[i][j] = card[i][j];
+//				if(flag == false)
+//				{
 					if(card[j][i] == 0)
 						continue;
 					else if(point == 3)
@@ -204,19 +261,19 @@ public class Cards implements Serializable
 					else if(temp[point + 1] == card[j][i])
 					{
 						temp[point + 1]++;
-						flag = true;
+//						flag = true;
 					}
 					else
 					{
 						temp[point] = card[j][i];
 						point--;
 					}
-				}
-				else
-				{
-					temp[point] = card[j][i];
-					point--;
-				}
+//				}
+//				else
+//				{
+//					temp[point] = card[j][i];
+//					point--;
+//				}
 			}
 			for(int j = 3; j >= 0; j--)
 			{
@@ -224,8 +281,23 @@ public class Cards implements Serializable
 				temp[j] = 0;
 			}
 		}
-//		rand = new Random(System.currentTimeMillis());
-		getRandom();
+		boolean change = false;
+		boolean full = true;
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				if(card[i][j] != last[i][j])
+					change = true;
+				if(card[i][j] == 0)
+					full = false;
+			}
+		}
+		if(change == true && full == false)
+		{
+			rand = new Random(System.currentTimeMillis());
+			getRandom();
+		}		
 	}
 	
 	public int getScore()
@@ -355,23 +427,23 @@ public class Cards implements Serializable
 		}
 		else
 		{
-			if(p < 0.6)
+			if(p < 0.8)
 				card[x][y] = 1;
-			else if(p < 0.8)
-				card[x][y] = 2;
 			else if(p < 0.9)
-				card[x][y] = 3;
+				card[x][y] = 2;
 			else if(p < 0.95)
-				card[x][y] = 4;
+				card[x][y] = 3;
 			else if(p < 0.975)
-				card[x][y] = 5;
+				card[x][y] = 4;
 			else if(p < 0.9875)
-				card[x][y] = 6;
+				card[x][y] = 5;
 			else if(p < 0.99375)
-				card[x][y] = 7;
+				card[x][y] = 6;
 			else if(p < 0.996875)
-				card[x][y] = 8;
+				card[x][y] = 7;
 			else if(p < 0.9984375)
+				card[x][y] = 8;
+			else if(p < 0.99921875)
 				card[x][y] = 9;
 			else
 				card[x][y] = 10;
